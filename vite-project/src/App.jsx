fb043cf38324a397dbaacc2167da39e85e3794f4
KK-2k06 <kumar.tech3005@gmail.com>
2025-07-27 01:28:47 +0530

    fix: renamed Home and NotFound components to PF-Home and PF-NotFound

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/home";
+import { Home } from "./pages/Home";
 import { NotFound } from "./pages/notfound";
 import {Toaster,toast} from 'react-hot-toast';
 
